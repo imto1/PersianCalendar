07c6275fc68119dd1d2b8a81d35b5dedbaed651a
imto1 <imto1.mail@gmail.com>
2020-02-27 00:23:35 +0330

    created python module

---
+++ python-module/persian_calendar.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+__author__ = "S. Vahid Hosseini"
+__license__ = "GPL-3.0"
+__version__ = "0.1"
+__maintainer__ = "S. Vahid Hosseini"
+__email__ = "s.vahid.h@behmerd.ir"
+__status__ = "Dev"
