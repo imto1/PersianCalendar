7c3be25dcc866c0632b2af8f1a1b5e27fabae4c8
imto1 <imto1.mail@gmail.com>
2020-02-28 11:33:18 +0330

    added methods to get today & year, month ,day for today

--- python-module/persian_calendar.py
+++ python-module/persian_calendar.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-import datetime
+from datetime import datetime
+from convert import to_persian
+from utilities import split_date
 
 __author__ = "S. Vahid Hosseini"
 __license__ = "GPL-3.0"
@@ -8,6 +10,34 @@ __maintainer__ = "S. Vahid Hosseini"
 __email__ = "s.vahid.h@behmerd.ir"
 __status__ = "Dev"
 
+def yoday():
+
+    calendar = datetime.today()
+    return  to_persian(calendar.year, calendar.month , calendar.day)
+
+
+def year():
+
+    calendar = datetime.today()
+    date = to_persian(calendar.year, calendar.month , calendar.day)
+    year = split_date(date)
+    return int(year[0])
+
+
+def month():
+
+    calendar = datetime.today()
+    date = to_persian(calendar.year, calendar.month , calendar.day)
+    month = split_date(date)
+    return int(month[1])
+
+
+def day():
+
+    calendar = datetime.today()
+    date = to_persian(calendar.year, calendar.month , calendar.day)
+    day = split_date(date)
+    return int(day[2])
 
 # TODO: remove followings before final release
 def main():
